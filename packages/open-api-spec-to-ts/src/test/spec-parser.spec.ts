95a2d267a516eb7ba01d0942a198bb37deea42a2
getlarge <ed@getlarge.eu>
2020-08-10 15:03:46 +0200

    Update spec-parser test suite

--- packages/open-api-spec-to-ts/src/test/spec-parser.spec.ts
+++ packages/open-api-spec-to-ts/src/test/spec-parser.spec.ts
@@ -2,15 +2,25 @@ import { readDir, removeFile } from '../files';
 import { generate } from '../spec-parser';
 
 describe('OpenAPISpecParser', () => {
-  const interfaceFilePath = './interfaces';
+    const interfaceFilePath = `${__dirname}/interfaces`;
 
-  afterAll(async () => {
-    const files = await readDir(interfaceFilePath);
-    Promise.all(files.map(async (file) => removeFile(file)));
-  });
+    afterAll(async () => {
+        const files = await readDir(interfaceFilePath);
+        return Promise.all(Object.values(files).map((file) => removeFile(`${interfaceFilePath}/${file}`)));
+    });
 
-  it('should generate from a valid Open API file', () => {
-    const testFile = 'petstore-expanded.json';
-    generate(testFile, interfaceFilePath);
-  });
+    it('should generate from a valid Open API file', async () => {
+        const testFile = `${__dirname}/uspto.json`;
+        await generate(testFile, interfaceFilePath);
+    });
+
+    it('should generate from a valid Open API file 2', async () => {
+        const testFile = `${__dirname}/petstore.json`;
+        await generate(testFile, interfaceFilePath);
+    });
+
+    it('should throw no such file or directory', async () => {
+        const invalidTestFile = `./invalid-path.json`;
+        await expect(generate(invalidTestFile, interfaceFilePath)).rejects.toThrow(`ENOENT: no such file or directory, open '${invalidTestFile}'`);
+    });
 });
