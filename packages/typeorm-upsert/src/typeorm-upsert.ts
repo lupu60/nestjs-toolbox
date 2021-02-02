302eb532c242269208d80b744c947dec85fc2c42
bogdanlupu <lupu60@gmail.com>
2021-02-02 22:11:24 +0100

    📦 NEW: add chunk option and set default chunk 1000

--- packages/typeorm-upsert/src/typeorm-upsert.ts
+++ packages/typeorm-upsert/src/typeorm-upsert.ts
@@ -9,27 +9,63 @@ import { Repository } from 'typeorm';
  * the row does not already exist but you do not want to overwrite this field if it already exists
  */
 export async function TypeOrmUpsert<T>(
-    repository: Repository<T>|Repository<any>|any,
-    object: T | T[],
-    conflictKey: string,
-    options?: {
-        keyNamingTransform?: (k: string) => string;
-        doNotUpsert?: string[];
-    },
+  repository: Repository<T> | Repository<any> | any,
+  object: T | T[],
+  conflictKey: string,
+  options?: {
+    keyNamingTransform?: (k: string) => string;
+    doNotUpsert?: string[];
+    chunk?: number;
+  },
 ): Promise<T[] | T | any> {
-    options = options ? options : {};
-    const keyNamingTransform = options.keyNamingTransform ?? ((k) => k);
-    const doNotUpsert = options.doNotUpsert ?? [];
-    const isCamelCase = (k: string) => /^[a-z]+[A-Z]/.test(k);
-    const sampleObject = Array.isArray(object) ? object[0] : object;
-    const keys: string[] = [Object.keys(sampleObject), doNotUpsert].reduce((a, b) => a.filter(c => !b.includes(c)));
-    const setterString = keys.map((k) => {
-        if (isCamelCase(k)) {
-            return `"${keyNamingTransform(k)}"=EXCLUDED."${k}"`;
-        }
-        return `${keyNamingTransform(k)}=EXCLUDED.${k}`;
-    });
-    const onConflict = `("${conflictKey}") DO UPDATE SET ${setterString.join(' , ')}`;
-    const qb = repository.createQueryBuilder().insert().values(object).onConflict(onConflict);
-    return (await qb.returning('*').execute()).raw;
+  options = options ? options : {};
+  const chunk = options.chunk ?? 1000;
+  const keyNamingTransform = options.keyNamingTransform ?? ((k) => k);
+  const doNotUpsert = options.doNotUpsert ?? [];
+  const sampleObject = Array.isArray(object) ? object[0] : object;
+  const keys: string[] = _keys({ sampleObject, doNotUpsert });
+  const setterString = _generateSetterString({ keys, keyNamingTransform });
+  const onConflict = `("${conflictKey}") DO UPDATE SET ${setterString}`;
+  const chunkedValues = _chunkValues({ values: object, chunk });
+  return (await _chunkPromises({ repository, chunkedValues, onConflict })).reduce((acc, current) => {
+    return acc.concat(current.raw);
+  }, []);
+}
+
+export async function _chunkPromises({ repository, chunkedValues, onConflict }) {
+  const promises = [];
+  for (let i = 0; i < chunkedValues.length; i++) {
+    const saveQuery = repository.createQueryBuilder().insert().values(chunkedValues[i]).onConflict(onConflict).returning('*').execute();
+    promises.push(
+      saveQuery.catch((e) => {
+        console.error(e);
+      }),
+    );
+  }
+  return await Promise.all(promises);
+}
+
+export function _chunkValues({ values, chunk }) {
+  const chunked_arr = [];
+  let copied = [...values];
+  const numOfChild = Math.ceil(copied.length / chunk);
+  for (let i = 0; i < numOfChild; i++) {
+    chunked_arr.push(copied.splice(0, chunk));
+  }
+  return chunked_arr;
+}
+
+export function _keys({ sampleObject, doNotUpsert }) {
+  return [Object.keys(sampleObject), doNotUpsert].reduce((a, b) => a.filter((c) => !b.includes(c)));
+}
+
+export function _generateSetterString({ keys, keyNamingTransform }) {
+  const isCamelCase = (k: string) => /^[a-z]+[A-Z]/.test(k);
+  const setterString = keys.map((k) => {
+    if (isCamelCase(k)) {
+      return `"${keyNamingTransform(k)}"=EXCLUDED."${k}"`;
+    }
+    return `${keyNamingTransform(k)}=EXCLUDED.${k}`;
+  });
+  return setterString.join(' , ');
 }
