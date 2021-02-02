3633e645b231cb3b999c32d93d4bb703588ce13c
bogdanlupu <lupu60@gmail.com>
2021-02-02 22:11:56 +0100

    ✅ TEST: typeupsert

--- packages/typeorm-upsert/src/test/typeorm-upsert.spec.ts
+++ packages/typeorm-upsert/src/test/typeorm-upsert.spec.ts
@@ -1,5 +1,83 @@
+import { _generateSetterString, TypeOrmUpsert, _keys, _chunkValues } from '../typeorm-upsert';
+
 describe('Dummy Test', () => {
-    it('should be defined', () => {
-        expect(true).toBeDefined();
+  it('should be defined', () => {
+    expect(TypeOrmUpsert).toBeDefined();
+  });
+
+  it('should generate setter string', () => {
+    const keys = ['id', 'name'];
+    const keyNamingTransform = (k) => k;
+    const expectedStatement = 'id=EXCLUDED.id , name=EXCLUDED.name';
+    expect(_generateSetterString({ keys, keyNamingTransform })).toEqual(expectedStatement);
+  });
+
+  it('should generate setter string', () => {
+    const keys = ['id', 'name', 'firstName'];
+    const keyNamingTransform = (k) => k;
+    const expectedStatement = 'id=EXCLUDED.id , name=EXCLUDED.name , "firstName"=EXCLUDED."firstName"';
+    expect(_generateSetterString({ keys, keyNamingTransform })).toEqual(expectedStatement);
+  });
+
+  it('should generate setter string with right key transform', () => {
+    const keys = ['id', 'name', 'first_name'];
+    const keyNamingTransform = (k) => k.toUpperCase();
+    const expectedStatement = 'ID=EXCLUDED.id , NAME=EXCLUDED.name , FIRST_NAME=EXCLUDED.first_name';
+    expect(_generateSetterString({ keys, keyNamingTransform })).toEqual(expectedStatement);
+  });
+
+  it('should generate the right keys', () => {
+    const data = { id: 1, name: '' };
+    expect(_keys({ sampleObject: data, doNotUpsert: [] })).toEqual(['id', 'name']);
+  });
+  it('should generate the right keys', () => {
+    const data = { id: 1, name: '', status: 'active' };
+    expect(_keys({ sampleObject: data, doNotUpsert: ['status'] })).toEqual(['id', 'name']);
+  });
+
+  it('should chunk huge arrays', () => {
+    const array = [
+      { id: 1, name: 'foo' },
+      { id: 2, name: 'bar' },
+      { id: 3, name: 'foo' },
+      { id: 4, name: 'bar' },
+    ];
+    const chunk = _chunkValues({ values: array, chunk: 2 });
+    expect(chunk.length).toEqual(2);
+  });
+
+  it('should save', async () => {
+    const repository = jest.fn();
+    // @ts-ignore
+    repository.createQueryBuilder = jest.fn().mockReturnValue({
+      insert: jest.fn().mockReturnValue({
+        values: jest.fn().mockReturnValue({
+          onConflict: jest.fn().mockReturnValue({ returning: jest.fn().mockReturnValue({ execute: jest.fn().mockResolvedValue({ raw: [] }) }) }),
+        }),
+      }),
     });
+    const data = [{ id: 1, name: '' }];
+    const saved = await TypeOrmUpsert(repository, data, 'id');
+    expect(saved).toBeDefined();
+  });
+
+  it('should save', async () => {
+    const repository = jest.fn();
+    // @ts-ignore
+    repository.createQueryBuilder = jest.fn().mockReturnValue({
+      insert: jest.fn().mockReturnValue({
+        values: jest.fn().mockReturnValue({
+          onConflict: jest.fn().mockReturnValue({ returning: jest.fn().mockReturnValue({ execute: jest.fn().mockResolvedValue({ raw: [] }) }) }),
+        }),
+      }),
+    });
+    const array = [
+      { id: 1, name: 'foo' },
+      { id: 2, name: 'bar' },
+      { id: 3, name: 'foo' },
+      { id: 4, name: 'bar' },
+    ];
+    const saved = await TypeOrmUpsert(repository, array, 'id', { chunk: 3 });
+    expect(saved).toBeDefined();
+  });
 });
