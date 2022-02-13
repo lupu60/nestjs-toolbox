ac4eda610413daca1607a24e6bcc0372658eb488
bogdanlupu <lupu60@gmail.com>
2022-02-13 19:25:33 +0100

    👌 IMPROVE: dep

--- packages/http-logger-middleware/src/http-logger-middleware.ts
+++ packages/http-logger-middleware/src/http-logger-middleware.ts
@@ -2,20 +2,25 @@ import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import * as chalk from 'chalk';
 @Injectable()
 export class HttpLoggerMiddleware implements NestMiddleware {
-    logger = new Logger('HTTP');
-    log = (...args) => this.logger.log(chalk.magenta(...args));
+  logger = new Logger('HTTP');
+  log = (...args) => this.logger.log(chalk.magenta(...args));
 
-    use(req: any, _res: any, next: () => void) {
-        const { body, params, query } = req;
-        const requestUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-
-        this.log('---------------------------------------------------------------------------------');
-        this.log('request method:', req.method);
-        this.log('request url: ', requestUrl);
-        this.log('request body: ', JSON.stringify(body));
-        this.log('request query: ', JSON.stringify(query));
-        this.log('request params: ', JSON.stringify(params));
-        this.log('---------------------------------------------------------------------------------');
-        next();
+  use(req: any, _res: any, next: () => void) {
+    const { body, params, query } = req;
+    const requestUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+    this.log('---------------------------------------------------------------------------------');
+    this.log('request method:', req.method);
+    this.log('request url: ', requestUrl);
+    if (query) {
+      this.log('request query: ', JSON.stringify(query));
     }
+    if (params) {
+      this.log('request params: ', JSON.stringify(params));
+    }
+    if (body) {
+      this.log('request body: ', JSON.stringify(body));
+    }
+    this.log('---------------------------------------------------------------------------------');
+    next();
+  }
 }
