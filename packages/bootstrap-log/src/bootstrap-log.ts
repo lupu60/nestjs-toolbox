229edc6eebf14d13ca2c452fca6a4703394bb31c
bogdanlupu <lupu60@gmail.com>
2022-02-13 19:31:07 +0100

    👌 IMPROVE: replace chalk

--- packages/bootstrap-log/src/bootstrap-log.ts
+++ packages/bootstrap-log/src/bootstrap-log.ts
@@ -1,5 +1,5 @@
 import { Logger } from '@nestjs/common';
-import chalk from 'chalk';
+import * as colors from 'colors';
 
 export interface AppConfig {
   environment: string;
@@ -19,22 +19,22 @@ export function BootstrapLog(options: { config: AppConfig }) {
   const { config } = options;
   const { redis_url, database_url, environment, hostname, package_json_body, health_check, swagger, sentry } = config;
   const logger: Logger = new Logger('Bootstrap');
-  logger.log(chalk.green.bold(`🎉 Bootstrapping ${package_json_body.name}:${package_json_body.version}`));
-  logger.log(chalk.red.bold(`🚀 Server is using ${environment} environment`));
-  logger.log(chalk.blue.bold(`✅ Server running on 👉 ${hostname}/api`));
+  logger.log(colors.green.bold(`🎉 Bootstrapping ${package_json_body.name}:${package_json_body.version}`));
+  logger.log(colors.red.bold(`🚀 Server is using ${environment} environment`));
+  logger.log(colors.blue.bold(`✅ Server running on 👉 ${hostname}/api`));
   if (database_url) {
-    logger.log(chalk.blue.bold(`💾 Database ${JSON.stringify({ url: database_url })}`));
+    logger.log(colors.blue.bold(`💾 Database ${JSON.stringify({ url: database_url })}`));
   }
   if (swagger) {
-    logger.log(chalk.green.bold(`📄 Swagger 👉 ${hostname}/swagger/`));
+    logger.log(colors.green.bold(`📄 Swagger 👉 ${hostname}/swagger/`));
   }
   if (health_check) {
-    logger.log(chalk.green.bold(`🩺 Check Health 👉 ${hostname}/health`));
+    logger.log(colors.green.bold(`🩺 Check Health 👉 ${hostname}/health`));
   }
   if (redis_url) {
-    logger.log(chalk.magenta.bold(`📮 Connected to ${redis_url}`));
+    logger.log(colors.magenta.bold(`📮 Connected to ${redis_url}`));
   }
   if (sentry) {
-    logger.log(chalk.blue.bold(`📶 Setting up Sentry for ${environment} environment`));
+    logger.log(colors.blue.bold(`📶 Setting up Sentry for ${environment} environment`));
   }
 }
